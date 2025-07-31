6ff7c6b1a6f212923112b56bcecbe24d84c13fa4
DESKTOP-2SHLKQ2\dino <dino@btnet.com.tw>
2025-07-31 16:28:26 +0800

    add deploy tool

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
   const isDev = command === 'serve';
 
   return {
-    base: isDev ? '/' : './',
+    base: isDev ? '/' : '/pixijs_practice/',
+    plugins: [],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
   }
 })
